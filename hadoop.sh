fab30d1ec90c1deb9721ed93ed789ae17af1f83e
Sam Davies <samdavies@live.co.uk>
2015-10-09 18:56:00 +0100

    Refactor hadoop job into separate .sh

--- hadoop.sh
+++ hadoop.sh
@@ -15,14 +15,7 @@ EOL
 echo ""
 echo "JOB COMMAND"
 echo "-----------"
-echo "hdfs dfs -rm -r /user/\$USER/data/output/; hadoop jar /opt/hadoop/hadoop-2.7.1/share/hadoop/tools/lib/hadoop-streaming-2.7.1.jar \
- -input /user/\$USER/data/input \
- -output /user/\$USER/data/output \
- -mapper mapper.py \
- -file Documents/EXC/Assignment1/$TASK/input/mapper.py \
- -reducer reducer.py \
- -file Documents/EXC/Assignment1/$TASK/input/reducer.py \
- -jobconf mapred.job.name=\"Sam's Word counter\""
+. $TASK/job.sh
 echo ""
 
 echo "COPY RESULTS COMMAND"
