04444a3916370ae4cb2af12b7fa2e4564c549e74
Sam Davies <samdavies@live.co.uk>
2015-10-27 00:09:48 +0000

    Create submission file

--- task1/job.sh
+++ task1/job.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 echo "hdfs dfs -rm -r /user/\$USER/data/output/ ;
-hdfs dfs -rm -r /user/\$USER/data/output/; hadoop jar /opt/hadoop/hadoop-2.7.1/share/hadoop/tools/lib/hadoop-streaming-2.7.1.jar \
+hdfs dfs -rm -r /user/\$USER/data/output/;
+hadoop jar /opt/hadoop/hadoop-2.7.1/share/hadoop/tools/lib/hadoop-streaming-2.7.1.jar \
  -input /data/assignments/ex1/webLarge.txt \
  -output /user/\$USER/data/output \
  -mapper mapper.py \
